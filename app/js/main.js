66eb38777673179d2474aa2809943bf984b84169
妞森 <niusen@niusentekiAir.Home>
2015-11-23 03:31:34 +0800

    add article

--- app/js/main.js
+++ app/js/main.js
@@ -3,21 +3,22 @@
 /*global  $, blueimp */
 
 $(function(){
-	console.log(0);
-
-	blueimp.Gallery($('#home-gallery .links'), {
-			container: '#blueimp-gallery-carousel',
-			carousel: true,
-			indicatorOptions: {
-				// The tag name, Id, element or querySelector of the indicator container:
-				indicatorContainer: 'ol',
-				// The class for the active indicator:
-				activeIndicatorClass: 'active',
-				// Defines if the gallery indicators should display a thumbnail:
-				thumbnailIndicators: false
+	if($('#home-gallery .links').length){
+		
+		blueimp.Gallery($('#home-gallery .links'), {
+				container: '#blueimp-gallery-carousel',
+				carousel: true,
+				indicatorOptions: {
+					// The tag name, Id, element or querySelector of the indicator container:
+					indicatorContainer: 'ol',
+					// The class for the active indicator:
+					activeIndicatorClass: 'active',
+					// Defines if the gallery indicators should display a thumbnail:
+					thumbnailIndicators: false
+				}
 			}
-		}
-	);
+		);
+	}
 	if($('.tablet,.mobile').length){
 		$('header nav').mmenu({});
 		var menu = $('.mm-menu').data( 'mmenu' );
